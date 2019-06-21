30da24ac7d533f5bc37564d927a58725188e5598
denis-kisel <denis.kisel92@gmail.com>
2019-06-21 17:39:17 +0300

    fix Image Install

--- src/Commands/InstallImageCommand.php
+++ src/Commands/InstallImageCommand.php
@@ -17,6 +17,6 @@ class InstallImageCommand extends Command
         file_exists(config_path('image.php')) || copy(__DIR__ . '/../../config/image.php', config_path('image.php'));
 
         File::makeDirectory(storage_path('app/public'), '775', true, true);
-        file_exists(storage_path('app/public/image.php')) || copy(__DIR__ . '/../../resources/image/placeholder.png', storage_path('app/public/image.php'));
+        file_exists(storage_path('app/public/placeholder.png')) || copy(__DIR__ . '/../../resources/image/placeholder.png', storage_path('app/public/placeholder.png'));
     }
 }
