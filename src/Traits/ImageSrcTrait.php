81ba5f7e599f47cd948e4f521c6f740c31ab35da
denis-kisel <denis.kisel92@gmail.com>
2019-11-05 16:41:58 +0200

    fix image src

--- src/Traits/ImageSrcTrait.php
+++ src/Traits/ImageSrcTrait.php
@@ -3,26 +3,14 @@
 
 namespace DenisKisel\Constructor\Traits;
 
-use DenisKisel\Constructor\Exceptions\ImageException;
 use DenisKisel\SmartImage\SmartImage;
 
 trait ImageSrcTrait
 {
-    public function imageSrc($size = null)
+    public function imageSrc($with = null, $height = null, $imageField = 'image')
     {
-        $sizes = [null, null];
-        if (!is_null($size)) {
-            if (is_string($size)) {
-                $sizes = config("image.sizes.{$size}");
-            } elseif (is_array($size) && count($size) == 2) {
-                $sizes = array_values($size);
-            } elseif (is_array($size) && count($size) != 2) {
-                throw new ImageException('Wrong parameter size! Array must be 2 elements! File: ' . __FILE__ . ' Line: ' . __LINE__);
-            }
-        }
+        $image = (empty($this->{$imageField}) || !file_exists(storage_path('app/public/' . $this->{$imageField}))) ? config('image.placeholder') : $this->{$imageField};
 
-        $image = (empty($this->image) || !file_exists(storage_path('app/public/' . $this->image))) ? config('image.placeholder') : $this->image;
-
-        return SmartImage::cache($image, $sizes[0], $sizes[1]);
+        return SmartImage::cache($image, $with, $height);
     }
 }
