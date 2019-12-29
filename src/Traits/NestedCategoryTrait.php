d1015fbcc80095cab659c9cf666826717f5e2872
Denis <denis.kisel92@gmail.com>
2019-12-29 10:55:01 +0200

    make cat path

--- src/Traits/NestedCategoryTrait.php
+++ src/Traits/NestedCategoryTrait.php
@@ -3,11 +3,22 @@
 namespace DenisKisel\Constructor\Traits;
 
 
+use App\Models\Category;
+use Tightenco\Collect\Support\Collection;
+
 trait NestedCategoryTrait
 {
-    public function getNestedAttribute()
+    /** @var Collection */
+    protected $pathByParent = null;
+
+    public function getNestedNameAttribute()
     {
         return $this->combine($this->id);
+    }
+
+    public function getPathAttribute()
+    {
+        return $this->path($this->id);
     }
 
     public function combine($id, $categories = [])
@@ -21,5 +32,26 @@ trait NestedCategoryTrait
             $categories = array_reverse($categories);
             return implode(' -> ', $categories);
         }
+    }
+
+    public function hasParent()
+    {
+        return (bool)$this->parent_id;
+    }
+
+    public function path($categoryId)
+    {
+        if (is_null($this->pathByParent)) {
+            $this->pathByParent = collect([]);
+        }
+        if (!is_null($categoryId)) {
+            $category = Category::find($categoryId);
+            $this->pathByParent->push($category);
+            if (!is_null($category->parent_id)) {
+                $this->path($category->parent_id);
+            }
+        }
+
+        return $this->pathByParent->reverse();
     }
 }
